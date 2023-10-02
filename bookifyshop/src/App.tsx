b2ace022259efea63fc97a7fadf39f4aec6b2888
Artem <overl0rdold@gmail.com>
2023-10-03 00:46:57 +0300

    first commit: init bookifyshop

---
+++ bookifyshop/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="app">
+        <header className="app__header">
+          Hello World!
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
